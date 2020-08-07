3ad3b80128fbca91c4f25f82d5b652d2f2a96e50
MiniCongee <replybber@gmail.com>
2020-08-07 20:54:37 +0800

    使用styled-components添加Footer、Header组件的CSS样式

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledFooter = styled.footer`
+  padding: 10px 100px;
+  text-align: center;
+  font-size: 12px;
+  color: #AAAAAA
+`
 
 function Footer() {
   return (
-    <footer>
+    <StyledFooter>
       <p>Footer</p>
-    </footer>
+    </StyledFooter>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,38 @@
 import React from 'react'
-import Logo from './logo.svg'
-import { Link } from 'react-router-dom'
+import LogoUrl from './logo.svg'
+import {NavLink} from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  padding: 10px 100px;
+  background-color: #02101F
+`
+const Logo = styled.img`
+  height: 30px;
+`
+// 修改子组件样式，需要将子组件传入
+const StyledLink = styled(NavLink)`
+  color: #fff;
+  margin-left: 30px;
+  
+  &.active {
+    border-bottom: 1px solid #FFFFFF;
+  }
+`
 
 function Header() {
   return (
-    <header>
-      <img src={Logo} alt=""/>
+    <StyledHeader>
+      <Logo src={LogoUrl}/>
       <nav>
-        <Link to="/">首页</Link>
-        <Link to="/history">上传历史</Link>
-        <Link to="/about">关于我</Link>
+        <StyledLink to="/" exact activeClassName="active">首页</StyledLink>
+        <StyledLink to="/history" activeClassName="active">上传历史</StyledLink>
+        <StyledLink to="/about" activeClassName="active">关于我</StyledLink>
       </nav>
-    </header>
+    </StyledHeader>
   )
 }
 
-export default Header;
+export default Header
