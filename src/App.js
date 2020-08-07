72660bacac2abc87725229cd0cc56649dae44826
MiniCongee <replybber@gmail.com>
2020-08-07 21:44:02 +0800

    增加Login、Register组件；增加stores

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import {
 const Home = lazy(() => import('./pages/Home'))
 const History = lazy(() => import('./pages/History'))
 const About = lazy(() => import('./pages/About'))
+const Login = lazy(() => import('./pages/Login'))
+const Register = lazy(() => import('./pages/Register'))
+
 
 function App() {
   return (
@@ -22,6 +25,8 @@ function App() {
             <Route path="/" exact component={Home}/>
             <Route path="/history" component={History}/>
             <Route path="/about" component={About}/>
+            <Route path="/login" component={Login}/>
+            <Route path="/register" component={Register}/>
           </Switch>
         </Suspense>
       </main>
