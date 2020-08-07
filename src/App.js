21985b1cf152d873e62e1027448ddb5e2a838afa
MiniCongee <replybber@gmail.com>
2020-08-07 16:33:32 +0800

    完成基础路由

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Suspense, lazy} from 'react'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Loading from './components/Loading'
+import {
+  Switch,
+  Route
+} from 'react-router-dom'
+
+const Home = lazy(() => import('./pages/Home'))
+const History = lazy(() => import('./pages/History'))
+const About = lazy(() => import('./pages/About'))
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+
+      <Suspense fallback={<div>Loading</div>}>
+
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/history" component={History}/>
+        <Route path="/about" component={About}/>
+      </Switch>
+      </Suspense>
+
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
