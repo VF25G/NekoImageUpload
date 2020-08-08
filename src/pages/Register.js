937b1799e076ac15ff21962a52882c6b216d1d73
MiniCongee <replybber@gmail.com>
2020-08-08 11:56:12 +0800

    引入antd完成Register基本布局

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,15 +1,100 @@
 import React from 'react'
-import { observer} from 'mobx-react'
-import { useStores} from '../stores'
+import {Form, Input, Button, Checkbox} from 'antd'
+import styled from 'styled-components'
 
-const Component = observer(() =>{
-  const { AuthStore } = useStores();
+const Wrapper = styled.div`
+  max-width: 600px;
+  margin: 30px auto;
+  box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  padding: 20px;
+`
+
+const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 30px;
+`
+
+const layout = {
+  labelCol: {
+    span: 4,
+  },
+  wrapperCol: {
+    span: 20,
+  },
+}
+const tailLayout = {
+  wrapperCol: {
+    offset: 4,
+    span: 20,
+  },
+}
+
+const Register = () => {
+  const onFinish = values => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo)
+  }
 
   return (
-    <>
-      <h1>Register:{AuthStore.values.username}</h1>
-    </>
-  )
-})
+    <Wrapper>
+      <Title>注册</Title>
+      <Form
+        {...layout}
+        name="basic"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="用户名"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: '请输入用户名',
+            },
+          ]}
+        >
+          <Input/>
+        </Form.Item>
 
-export default Component;
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: '请输入密码',
+            },
+          ]}
+        >
+          <Input.Password/>
+        </Form.Item>
+
+        <Form.Item
+          label="确认密码"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: '再次确认密码',
+            },
+          ]}
+        >
+          <Input.Password/>
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            提交
+          </Button>
+        </Form.Item>
+      </Form>
+    </Wrapper>
+  )
+}
+
+export default Register
