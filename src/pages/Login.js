c163bd6e5b656bd77c44fbb6a51c4d55aa9f2085
MiniCongee <replybber@gmail.com>
2020-08-08 16:16:01 +0800

    简易创建Login页面

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,104 @@
-import React, { useRef } from 'react'
-import { observer} from 'mobx-react'
-import { useStores} from '../stores'
+import React from 'react'
+import { Form, Input, Button } from 'antd'
+import styled from 'styled-components'
 
-const Component = observer(() =>{
-  const { AuthStore } = useStores();
-  const inputRef = useRef()
+const Wrapper = styled.div`
+  max-width: 600px;
+  margin: 30px auto;
+  box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  padding: 20px;
+`
 
-  const bingChange = e => {
-    console.log(inputRef.current.value)
-    AuthStore.setUsername(inputRef.current.value)
+const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 30px;
+`
+
+const layout = {
+  labelCol: {
+    span: 6,
+  },
+  wrapperCol: {
+    span: 18,
+  },
+}
+const tailLayout = {
+  wrapperCol: {
+    offset: 6,
+    span: 18,
+  },
+}
+
+const Register = () => {
+  const onFinish = values => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo)
+  }
+
+  const validateUsername = (rule, value) => {
+    if (/\W/.test(value)) { return Promise.reject('只能是字母数字下划线')}
+    if (value.length < 4 || value.length > 12) { return Promise.reject('用户名长度为4～12个字符')}
+    return Promise.resolve()
   }
 
   return (
-    <>
-      <h1>Login:{AuthStore.values.username}</h1>
-      <input onChange={bingChange} ref={inputRef} />
-    </>
-  )
-})
+    <Wrapper>
+      <Title>登录</Title>
+      <Form
+        {...layout}
+        name="basic"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="用户名"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: '请输入用户名',
+            },
+            {
+              validator: validateUsername
+            }
+          ]}
+        >
+          <Input/>
+        </Form.Item>
 
-export default Component;
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: '请输入密码',
+            },
+            {
+              min: 8,
+              message: '密码不少于8个字符'
+            },
+            {
+              max: 24,
+              message: '密码不多于24个字符'
+            }
+          ]}
+        >
+          <Input.Password/>
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            提交
+          </Button>
+        </Form.Item>
+      </Form>
+    </Wrapper>
+  )
+}
+
+export default Register
