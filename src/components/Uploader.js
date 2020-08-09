6242a770eba1d278b19221499835df981e505854
MiniCongee <replybber@gmail.com>
2020-08-09 16:46:06 +0800

    上传结果展示

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -3,8 +3,22 @@ import {useStores} from '../stores'
 import {observer} from 'mobx-react'
 import {message, Upload} from 'antd'
 import { InboxOutlined } from '@ant-design/icons';
+import styled from 'styled-components'
 
 const { Dragger } = Upload;
+
+const Result = styled.div`
+  margin-top: 30px;
+  border: 1px dashed #ccc;
+  padding: 20px;
+`
+const H1 = styled.h1`
+  margin: 20px 0;
+  text-align: center;
+`
+const Image = styled.img`
+max-width: 300px;
+`
 
 const Component = observer(() => {
   const {ImageStore, UserStore} = useStores()
@@ -40,15 +54,27 @@ const Component = observer(() => {
           band files
         </p>
       </Dragger>
-      <div>
-        <h1>上传结果</h1>
-        {
-          ImageStore.serverFile ?
-            <div>
-              {ImageStore.serverFile.attributes.url.attributes.url}
-            </div> : null
-        }
-      </div>
+      {
+        ImageStore.serverFile ? <Result>
+          <H1>上传结果</H1>
+          <dl>
+            <dt>链接</dt>
+            <dd><a target="_blank" href={ ImageStore.serverFile.attributes.url.attributes.url }>{ ImageStore.serverFile.attributes.url.attributes.url }</a></dd>
+            <dt>文件名</dt>
+            <dd>{ImageStore.filename}</dd>
+            <dt>图片预览</dt>
+            <dd>
+              <Image src={ ImageStore.serverFile.attributes.url.attributes.url } alt=""/>
+            </dd>
+            <dt>更多尺寸</dt>
+            <dd>
+              <input placeholder="最大宽度（可选）"/>
+              <input placeholder="最大高度（可选）"/>
+            </dd>
+          </dl>
+        </Result> :
+          null
+      }
     </div>
   )
 })
