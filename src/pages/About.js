884e2823a290551006c7de4a2ed5d9309fe2e230
MiniCongee <replybber@gmail.com>
2020-09-10 15:55:47 +0800

    初步完善About页面

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+import LogoUrl from '../components/logo.svg'
+
+const StyledH1 = styled.h1`
+  margin-bottom: 0;
+`
+
+const Logo = styled.img`
+  height: 120px;
+  margin: 0 auto;
+  display: block;
+`
 
 function About() {
   return (
     <>
-      <h1>About</h1>
+      <StyledH1>About</StyledH1>
+      <Logo src={LogoUrl}/>
+      <p>Neko Image Upload基于React进行开发实现。</p>
+      <p>依赖LeanCloud作为后台服务，使用Mobx进行数据管理。</p>
+      <p>本图床作为学习React过程中所产生的副产物，并不保证图床链接长期有效。</p>
     </>
   )
 }
--- src/pages/History.js
+++ src/pages/History.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import List from '../components/List'
+import styled from 'styled-components'
+
+
+const StyledH1 = styled.h1`
+  margin-bottom: 0;
+`
 
 function History() {
   return (
     <>
-      <h1>History</h1>
+      <StyledH1>History</StyledH1>
       <List/>
     </>
   )
